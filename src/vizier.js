fc93b0e35e2ce45c0969027bce218535f3532b43
Jo Bovy <bovy@astro.utoronto.ca>
2020-10-18 21:20:04 -0400

    Tweaks to allow a single file with ReadMe + data, to allow files at a general URL (not Vizier), and to fix parsing of empty data lines

--- src/vizier.js
+++ src/vizier.js
@@ -33,13 +33,17 @@
 	const re= RegExp(/\s*(?<start>\d+\s*\-)?\s*(?<end>\d+)\s+(?<format>[\w.]+)\s+(?<units> \S+)\s+(?<name>\S+)(\s+(?<descr>\S.*))?/);
 	for ( let ii=0; ii < lines.length; ii++ ) {
 	    line= lines[ii];
-	    if ( line.startsWith(cat) ) {
+	    if ( cat && line.startsWith(cat) ) {
 		header['title']= line.split('(')[0].split(cat)[1].trim();
 		header['author']= line.split('(')[1].split(',')[0];
 		header['year']= line.split('(')[1].split(',')[1].split(')')[0];
+	    } else if (line.startsWith('Title') ) {
+		header['title']= line.split('Title:').slice(-1)[0].trim();
+	    } else if (line.startsWith('Authors') ) {
+		header['author']= line.split('Authors:').slice(-1)[0].trim();
 	    }
 	    if ( line.startsWith('Byte-by-byte Description of file') &&
-		 line.indexOf(datafile) !== -1 ) {
+		 line.indexOf(datafile.split('/').slice(-1)[0]) !== -1 ) {
 		read_col_desc= true;
 	    }
 	    if ( read_col_desc && ( line.startsWith('------------')
@@ -91,6 +95,8 @@
 	// Process data file line by line
 	let val;
 	data.split('\n').forEach(function (line) {
+	    if ( line == '' || line == '\r' )
+		return null;
 	    header['splitter'].forEach(function (col) {
 		val= line.slice(col['BytesStart'],col['BytesEnd'])
 		if ( col['Format'][0] == 'A' )
@@ -117,22 +123,35 @@
 	// ReadMe given?
 	ReadMe = ((typeof ReadMe !== 'undefined') ? 
                   ReadMe : null);
+	// cat given?
+	let data_url;
+	let readme_url;
+	if ( cat === null ) {
+	    data_url= datafile;
+	    readme_url= ReadMe;
+	} else {
+	    data_url= `https://cdsarc.unistra.fr/ftp/${cat}/${datafile}`;
+	    readme_url= `https://cdsarc.unistra.fr/ftp/${cat}/${ReadMe}`;
+	}
 	
 	let jqXHR;
 	if ( ReadMe !== null )
-	    jqXHR= $.when($.get('https://cdsarc.unistra.fr/ftp/'
-				+`${cat}/${datafile}`),
-			  $.get('https://cdsarc.unistra.fr/ftp/'
-				+`${cat}/${ReadMe}`))
+	    jqXHR= $.when($.get(data_url),$.get(readme_url))
 	    .then(function (data,readme) {
 		return [data[0],readme[0]];
             });
 	else
-	    jqXHR= $.get(`https://cdsarc.unistra.fr/ftp/${cat}/${datafile}`)
+	    jqXHR= $.get(data_url)
 	    .then(function (data) {
 		// Extract readme from data file
-		Promise.reject('Extracting the readme from the datafile itself is not implemented yet');
-		return [data,undefined];
+		let readme= data.split(data.split('--------------------------------------------------------------------------------').slice(-1))
+		data= data.split('--------------------------------------------------------------------------------').slice(-1);
+		// Make sure that the filename in the ReadMe matches that of
+		// the file (doesn't always)
+		const re_filename= RegExp(/Byte-by-byte Description of file: (?<filename>\S+)/);
+		readme= readme[0].replace(`Byte-by-byte Description of file: ${re_filename.exec(readme).groups.filename}`,
+					  `Byte-by-byte Description of file: ${datafile.split('/').slice(-1)[0]}`);
+		return [data[0],readme];
 	    });
 
 	return jqXHR.then(function (dataAndreadme) {
